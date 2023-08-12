c1d31e3673402d7fd430088730019c55a53cf5b7
samuelT7 <samueltadele07@gmail.com>
2023-08-12 14:57:16 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - A c program that prints a line to the standard error
- * Return: 1 (Success)
+ * main - A c program that prints a qoute using write function
+ *
+ * Return: 1 (error)
  */
+
 int main(void)
-{
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout)
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
 	return (1);
 }
