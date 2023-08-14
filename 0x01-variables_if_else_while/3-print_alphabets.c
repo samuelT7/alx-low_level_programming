13aca98d647b8811c0531a0586fc4361fcac70cd
samuelT7 <samueltadele07@gmail.com>
2023-08-14 13:26:04 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - A C program that prints alphabetic small and capital letters
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char sc;
+	for (sc = 'a'; sc <= 'z'; sc++)
+	putchar(sc);
+
+	for (sc = 'A'; sc <= 'Z'; sc++)
+	putchar(sc);
+	putchar('\n');
+
+	return (0);
+}
