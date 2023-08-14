60250ce582616812d987774260dc1a7a52901275
samuelT7 <samueltadele07@gmail.com>
2023-08-14 13:54:42 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - A c  program that prints all single digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	printf("%d", n);
+	printf("\n");
+
+	return (0);
+}
