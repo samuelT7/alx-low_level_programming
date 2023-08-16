d2b246ae2666bb581fd1cf4d4c595f1fdcdda998
samuelT7 <samueltadele07@gmail.com>
2023-08-15 19:40:41 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+	long b = 1, g = 2;
+
+	while (n < 50)
+	{
+	if (n == 0)
+	printf("%ld", b);
+	else if (n == 1)
+	printf(", %ld", g);
+	else
+	{
+	g += b;
+	b = g - b;
+	printf(", %lb", g);
+	}
+	n++;
+	}
+	printf("\n");
+	return (0);
+}
